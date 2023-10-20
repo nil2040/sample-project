8a1d37d8617636fba7795cfa82f0115e245881d5
nil2040 <rganguli08@gmail.com>
2023-10-20 23:49:02 +0530

    First commit

---
+++ test.java
@@ -0,0 +1,15 @@
+class add
+{
+public static void main(String[]args)
+{
+int a,b,c,d,e;
+
+a=Integer.parseInt(args[0]);
+b=Integer.parseInt(args[1]);
+c=Integer.parseInt(args[2]);
+d=Integer.parseInt(args[3]);
+e=Integer.parseInt(args[4]);
+System.out.print("sum="+(a+b+c+d+e));
+System.out.print("Percentage="+(a+b+c+d+e)*100/500);
+}
+}
