4fa07dc3040b1406066ccccfeea82a50349624af
nil2040 <rganguli08@gmail.com>
2023-10-21 10:57:00 +0530

    changes made in demo.java updated no variable

--- demo.java
+++ demo.java
@@ -5,8 +5,7 @@ public static void main(String[]args)
 int a,b,c,d,e;
 
 
-d=Integer.parseInt(args[3]);
-e=Integer.parseInt(args[4]);
+
 System.out.print("sum="+(a+b+c+d+e));
 System.out.print("Percentage="+(a+b+c+d+e)*100/500);
 }
