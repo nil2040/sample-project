9c400b41304b7f420b20dcf35c600a5c22a6c567
nil2040 <rganguli08@gmail.com>
2023-10-21 10:36:00 +0530

    changes made in demo.java

--- demo.java
+++ demo.java
@@ -4,9 +4,7 @@ public static void main(String[]args)
 {
 int a,b,c,d,e;
 
-a=Integer.parseInt(args[0]);
-b=Integer.parseInt(args[1]);
-c=Integer.parseInt(args[2]);
+
 d=Integer.parseInt(args[3]);
 e=Integer.parseInt(args[4]);
 System.out.print("sum="+(a+b+c+d+e));
